05a9e6b811eb19c12565719f4e4ac8d4285ab307
Vlad Ureche <vlad.ureche@gmail.com>
2014-07-07 22:36:34 +0200

    Explored the combination inlined + TL return
    Ping @siriux

--- src/main/scala/valium/benchmark/MultivalueBenchmark.scala
+++ src/main/scala/valium/benchmark/MultivalueBenchmark.scala
@@ -1,5 +1,6 @@
 package valium.benchmark
 import org.scalameter.api._
+import org.scalameter.execution.SeparateJvmsExecutor
 
 trait MultiValueStorage {
   var val1: Long = 0
@@ -48,7 +49,7 @@ object MultivalueBenchmark extends PerformanceTest {
   }
 
   // Using ThreadLocal
-  val _storage = new ThreadLocal[MultiValueStorage]
+  lazy val _storage = new ThreadLocal[MultiValueStorage]
   def getStorage(): MultiValueStorage = {
     val s = _storage.get()
     if (s != null) {
@@ -60,8 +61,43 @@ object MultivalueBenchmark extends PerformanceTest {
     }
   }
 
-  var rs: Long = 1
-  var rp: Long = 2
+  var rs1: Long = 1
+  var rp1: Long = 2
+
+  def fMultivalueReturn(val1: Long, val2: Long, val3: Long): MultiValueStorage = {
+    val a = val1
+    val b = val2
+    val c = val3
+
+    val s = getStorage()
+    s.val1 = a+b+c
+    s.val2 = a*b*c
+
+    s
+  }
+
+  performance of "MultivalueReturn" in {
+    using(Gen.unit("")) in {
+      (_) => {
+        var i = NumIterations
+        while (i > 0) {
+          val val1 = rs1
+          val val2 = rp1
+          val val3 = rs1 + rp1
+
+          val s2 = fMultivalueReturn(val1, val2, val3)
+
+          rs1 = s2.val1
+          rp1 = s2.val2
+
+          i -= 1
+        }
+      }
+    }
+  }
+
+  var rs2: Long = 1
+  var rp2: Long = 2
 
   def fMultivalue(s: MultiValueStorage): MultiValueStorage = {
     val a = s.val1
@@ -80,14 +116,14 @@ object MultivalueBenchmark extends PerformanceTest {
         var i = NumIterations
         while (i > 0) {
           val s1 = getStorage()
-          s1.val1 = rs
-          s1.val2 = rp
-          s1.val3 = rs + rp
+          s1.val1 = rs2
+          s1.val2 = rp2
+          s1.val3 = rs2 + rp2
 
           val s2 = fMultivalue(s1)
 
-          rs = s2.val1
-          rp = s2.val2
+          rs2 = s2.val1
+          rp2 = s2.val2
 
           i -= 1
         }
@@ -102,14 +138,14 @@ object MultivalueBenchmark extends PerformanceTest {
         val s = getStorage()
         while (i > 0) {
           val s1 = s
-          s1.val1 = rs
-          s1.val2 = rp
-          s1.val3 = rs + rp
+          s1.val1 = rs2
+          s1.val2 = rp2
+          s1.val3 = rs2 + rp2
 
           val s2 = fMultivalue(s1)
 
-          rs = s2.val1
-          rp = s2.val2
+          rs2 = s2.val1
+          rp2 = s2.val2
 
           i -= 1
         }
@@ -117,95 +153,95 @@ object MultivalueBenchmark extends PerformanceTest {
     }
   }
 
-  var ov3: Long = 0
-  var ov4: Long = 0
-  var ov5: Long = 0
-  var or1: Object = _
-  var or2: Object = _
-  var or3: Object = _
-  var or4: Object = _
-  var or5: Object = _
-  val ref: Object = ""
-
-  performance of "Multivalue Generic Overhead 5" in {
-    using(Gen.unit("")) in {
-      (_) => {
-        var i = NumIterations
-        while (i > 0) {
-          val s1 = getStorage()
-          s1.val1 = rs
-          s1.val2 = rp
-          s1.val3 = rs + rp
-
-          // Generic Overhead
-          s1.val4 = -1
-          s1.val5 = -1
-          s1.ref1 = ref
-          s1.ref2 = ref
-          s1.ref3 = ref
-          s1.ref4 = ref
-          s1.ref5 = ref
-
-          val s2 = fMultivalue(s1)
-
-          rs = s2.val1
-          rp = s2.val2
-
-          // Generic Overhead
-          ov3 = s2.val3
-          ov4 = s2.val4
-          ov5 = s2.val5
-          or1 = s2.ref1
-          or2 = s2.ref2
-          or3 = s2.ref3
-          or4 = s2.ref4
-          or5 = s2.ref5
-
-          i -= 1
-        }
-      }
-    }
-  }
-
-  performance of "Multivalue Generic Overhead 5 Reuse" in {
-    using(Gen.unit("")) in {
-      (_) => {
-        var i = NumIterations
-        val s = getStorage()
-        while (i > 0) {
-          val s1 = s
-          s1.val1 = rs
-          s1.val2 = rp
-          s1.val3 = rs + rp
-
-          // Generic Overhead
-          s1.val4 = -1
-          s1.val5 = -1
-          s1.ref1 = ref
-          s1.ref2 = ref
-          s1.ref3 = ref
-          s1.ref4 = ref
-          s1.ref5 = ref
-
-          val s2 = fMultivalue(s1)
-
-          rs = s2.val1
-          rp = s2.val2
-
-          // Generic Overhead
-          ov3 = s2.val3
-          ov4 = s2.val4
-          ov5 = s2.val5
-          or1 = s2.ref1
-          or2 = s2.ref2
-          or3 = s2.ref3
-          or4 = s2.ref4
-          or5 = s2.ref5
-
-          i -= 1
-        }
-      }
-    }
-  }
+//  var ov3: Long = 0
+//  var ov4: Long = 0
+//  var ov5: Long = 0
+//  var or1: Object = _
+//  var or2: Object = _
+//  var or3: Object = _
+//  var or4: Object = _
+//  var or5: Object = _
+//  val ref: Object = ""
+//
+//  performance of "Multivalue Generic Overhead 5" in {
+//    using(Gen.unit("")) in {
+//      (_) => {
+//        var i = NumIterations
+//        while (i > 0) {
+//          val s1 = getStorage()
+//          s1.val1 = rs
+//          s1.val2 = rp
+//          s1.val3 = rs + rp
+//
+//          // Generic Overhead
+//          s1.val4 = -1
+//          s1.val5 = -1
+//          s1.ref1 = ref
+//          s1.ref2 = ref
+//          s1.ref3 = ref
+//          s1.ref4 = ref
+//          s1.ref5 = ref
+//
+//          val s2 = fMultivalue(s1)
+//
+//          rs = s2.val1
+//          rp = s2.val2
+//
+//          // Generic Overhead
+//          ov3 = s2.val3
+//          ov4 = s2.val4
+//          ov5 = s2.val5
+//          or1 = s2.ref1
+//          or2 = s2.ref2
+//          or3 = s2.ref3
+//          or4 = s2.ref4
+//          or5 = s2.ref5
+//
+//          i -= 1
+//        }
+//      }
+//    }
+//  }
+//
+//  performance of "Multivalue Generic Overhead 5 Reuse" in {
+//    using(Gen.unit("")) in {
+//      (_) => {
+//        var i = NumIterations
+//        val s = getStorage()
+//        while (i > 0) {
+//          val s1 = s
+//          s1.val1 = rs
+//          s1.val2 = rp
+//          s1.val3 = rs + rp
+//
+//          // Generic Overhead
+//          s1.val4 = -1
+//          s1.val5 = -1
+//          s1.ref1 = ref
+//          s1.ref2 = ref
+//          s1.ref3 = ref
+//          s1.ref4 = ref
+//          s1.ref5 = ref
+//
+//          val s2 = fMultivalue(s1)
+//
+//          rs = s2.val1
+//          rp = s2.val2
+//
+//          // Generic Overhead
+//          ov3 = s2.val3
+//          ov4 = s2.val4
+//          ov5 = s2.val5
+//          or1 = s2.ref1
+//          or2 = s2.ref2
+//          or3 = s2.ref3
+//          or4 = s2.ref4
+//          or5 = s2.ref5
+//
+//          i -= 1
+//        }
+//      }
+//    }
+//  }
 
 }
